a2c814b422826750b69ede628b685876d4793091
ShubhamKumar37 <sk9818281820@gmail.com>
2024-09-04 17:40:47 +0530

    first commit and few files regarding to javaScript

---
+++ 01_Basics/01_variable.js
@@ -0,0 +1,11 @@
+const accountId = 1234;
+let email = "sk@gmail.com";
+var name = "Shubham Kumar";
+city = "Delhi";
+let state;
+
+// accountId = 4321;
+// email = "shubham@gmail.com";
+name = "Sparsh"
+
+console.table([accountId, email, name, city, state]);
---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,14 @@
+"use strict" // By writting this my whole code will be treated as in newer version of javascript
+
+// alert(21 + 1); // This will not work because we are working a enviroment not in a file which will executing on browser
+
+// number
+// boolean
+// string
+// null
+// undefined
+// symbol - uniqueness
+// object - collection of key value pairs
+
+console.log(typeof null); // <- This is a object
+console.log(typeof undefined);
