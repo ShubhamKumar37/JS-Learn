e193fe4a1ed8b5addb218607d767126d5a702b90
ShubhamKumar37 <sk9818281820@gmail.com>
2024-09-17 20:39:58 +0530

    control flow statement

---
+++ 04_Control_Flow/01_if_else_elseif.js
@@ -0,0 +1,12 @@
+if(2 == 1)
+{
+    console.log("Both value are equal");
+}
+else
+{
+    console.log("Not equal");
+}
+
+console.log(2 == "2")
+console.log(2 === "2")
+// <, >, <=, >=, !=, ===, !==, &&, ||
