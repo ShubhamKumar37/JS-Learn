b9e50ca18944e6ae962e5e6f8bb0c810f88715b9
ShubhamKumar37 <sk9818281820@gmail.com>
2024-09-21 19:49:06 +0530

    new topic loops

--- 04_Control_Flow/01_if_else_elseif.js
+++ 04_Control_Flow/01_if_else_elseif.js
@@ -1,12 +1,26 @@
-if(2 == 1)
-{
+if (2 == 1) {
     console.log("Both value are equal");
 }
-else
-{
+else {
     console.log("Not equal");
 }
 
 console.log(2 == "2")
 console.log(2 === "2")
 // <, >, <=, >=, !=, ===, !==, &&, ||
+
+let a = 31;
+switch (a) {
+    case 2:
+        console.log("This is 2");
+        break;
+    case 3:
+        console.log("This is 3");
+        break;
+    case 6:
+        console.log("This is 8");
+        break;
+
+    default:
+        console.log("This is default case");
+}
