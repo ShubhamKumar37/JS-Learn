2bed4c3749fd464b33d26a21a74ac2c4d882f131
ShubhamKumar37 <sk9818281820@gmail.com>
2024-09-22 20:54:32 +0530

    more about forEach and filter function in js

---
+++ 05_Iterations/04_ArraySpecifedLoop2.js
@@ -0,0 +1,43 @@
+const arr1 = ["shubham", "kumar", "singh", "jain", "sharma", "gupta"];
+
+// const value = arr1.forEach((item) => 
+// {
+//     console.log(item);
+//     return item;
+// });
+
+// console.log(value);
+
+const arr2 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// const arr3 = arr2.filter((item) => item > 4);
+// const arr3 = arr2.filter((item) => 
+// {
+//     return item > 4;
+// });
+// console.log(arr3)
+
+const arr4 = [
+    {name: "Shubham Kumar", title: "Software Engineer", year: 2020, salary: 150000},
+    {name: "Nikhil Jain", title: "Data Scientist", year: 2019, salary: 200000},
+    {name: "Aman Gupta", title: "Devops Engineer", year: 2018, salary: 180000},
+    {name: "Rahul Singh", title: "Cloud Engineer", year: 2021, salary: 250000},
+    {name: "Raj Kumar", title: "Full Stack Developer", year: 2017, salary: 120000},
+    {name: "Kumar Shubham", title: "Backend Developer", year: 2016, salary: 100000},
+    {name: "Jain Nikhil", title: "Machine Learning Engineer", year: 2015, salary: 220000},
+    {name: "Gupta Aman", title: "Frontend Developer", year: 2014, salary: 150000},
+    {name: "Singh Rahul", title: "Cybersecurity Engineer", year: 2013, salary: 280000},
+    {name: "Kumar Raj", title: "Database Administrator", year: 2012, salary: 130000},
+    {name: "Shubham Kumar", title: "Network Engineer", year: 2011, salary: 160000},
+    {name: "Nikhil Jain", title: "Cloud Architect", year: 2010, salary: 300000},
+    {name: "Aman Gupta", title: "Solutions Architect", year: 2009, salary: 350000},
+    {name: "Rahul Singh", title: "Enterprise Architect", year: 2008, salary: 400000},
+    {name: "Raj Kumar", title: "IT Consultant", year: 2007, salary: 500000}
+];
+
+const arr3 = arr4.filter((item) =>
+{
+    return item.salary > 200000;
+});
+
+console.table(arr3);
